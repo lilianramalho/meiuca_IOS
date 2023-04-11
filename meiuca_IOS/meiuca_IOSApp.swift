6acef2a988af765e8372b6e7db714b1a74aa1455
Lílian Carneiro Ramalho <60183687+lilianramalho@users.noreply.github.com>
2023-04-10 21:53:06 -0300

    Initial Commit

---
+++ meiuca_IOS/meiuca_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  meiuca_IOSApp.swift
+//  meiuca_IOS
+//
+//  Created by Lilian Carneiro Ramalho on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct meiuca_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meiuca_IOSUITests/meiuca_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  meiuca_IOSUITestsLaunchTests.swift
+//  meiuca_IOSUITests
+//
+//  Created by Lilian Carneiro Ramalho on 10/04/23.
+//
+
+import XCTest
+
+final class meiuca_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
